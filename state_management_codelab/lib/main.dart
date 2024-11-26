33d6fe053088fdb590c4f707ec35184cf0d404e8
liwangivone <liwangivone@gmail.com>
2024-11-26 23:23:10 +0800

    include stateful widget version

--- state_management_codelab/lib/main.dart
+++ state_management_codelab/lib/main.dart
@@ -1,38 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyStatelessApp());
+  runApp(const MyStatelessApp());
 }
 
 class MyStatelessApp extends StatelessWidget {
+  const MyStatelessApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Stateless Counter App')),
-        body: CounterWidget(),
+        appBar: AppBar(title: const Text('Stateless Counter App')),
+        body: const CounterWidget(),
       ),
     );
   }
 }
 
 class CounterWidget extends StatelessWidget {
+  const CounterWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text('Counter Value: 0'),
-          SizedBox(height: 10),
+          const Text('Counter Value: 0'),
+          const SizedBox(height: 10),
           ElevatedButton(
             onPressed: () {
               // TODO: Increment counter
             },
-            child: Text('Increment'),
+            child: const Text('Increment'),
           ),
         ],
       ),
     );
   }
 }
+
+class MyStatefulWidgetApp extends StatefulWidget {
+  const MyStatefulWidgetApp({super.key});
+
+  @override
+  MyStatefulWidgetAppState createState() => MyStatefulWidgetAppState();
+}
+
+class MyStatefulWidgetAppState extends State<MyStatefulWidgetApp> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Stateful Counter App')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Counter Value: $counter'),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: incrementCounter,
+                child: const Text('Increment'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+  // Uncomment the following line to run the StatefulWidget version:
+  // runApp(MyStatefulWidgetApp());
