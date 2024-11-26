ebfc1991b4f6f178e369b556ed1d45e1fcf9018a
liwangivone <liwangivone@gmail.com>
2024-11-26 23:16:25 +0800

    stateless widget

---
+++ state_management_codelab/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyStatelessApp());
+}
+
+class MyStatelessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Stateless Counter App')),
+        body: CounterWidget(),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Counter Value: 0'),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              // TODO: Increment counter
+            },
+            child: Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
